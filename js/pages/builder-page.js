b6d540e72509fe05559e387e29124ef31af220a3
raluca-korean <raluca.elena.macovei@gmail.com>
2026-03-19 16:36:25 +0200

    Update builder-page.js

--- js/pages/builder-page.js
+++ js/pages/builder-page.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+  try {
+
+    await loadVocabulary();
+
+    initBuilderPage();
+
+  } catch(err){
+
+    console.error("BOOT ERROR", err);
+
+  }
+
+});
+
+
+function initBuilderPage(){
+
+  sentences = [ makeEmptySentence() ];
+  actives = [ makeAllActive() ];
+
+  loadModelRow(indexModelRow);
+
+  setupUI();
+  createToggleChips();
+
+  attachPressHandlers(DOM.tableP1,0);
+  attachPressHandlers(DOM.tableP2,1);
+
+  renderAll();
+
+}
