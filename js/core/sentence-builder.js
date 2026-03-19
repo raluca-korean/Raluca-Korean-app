18f90f302ab79d5a52e9307ed63f216e81259cb5
raluca-korean <raluca.elena.macovei@gmail.com>
2026-03-19 16:45:39 +0200

    Update sentence-builder.js

--- js/core/sentence-builder.js
+++ js/core/sentence-builder.js
@@ -1,1 +1,24 @@
+let sentences = [];
+let actives = [];
 
+function makeEmptySentence(){
+  return {
+    subject: "",
+    verb: "",
+    object: "",
+    time: ""
+  };
+}
+
+function makeAllActive(){
+  return {
+    subject: true,
+    verb: true,
+    object: true,
+    time: true
+  };
+}
+
+function renderAll(){
+  console.log("Rendering sentences:", sentences);
+}
