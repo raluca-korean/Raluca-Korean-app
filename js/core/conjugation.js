a4d3d7060791354f487001858089976ae1af688e
raluca-korean <raluca.elena.macovei@gmail.com>
2026-03-19 17:14:39 +0200

    Update conjugation.js

--- js/core/conjugation.js
+++ js/core/conjugation.js
@@ -1,3 +1,34 @@
-function conjugate(verb){
+function getVerbStem(verb){
+  return verb.replace("다", "");
+}
+
+function presentPolite(verb){
+  const stem = getVerbStem(verb);
+
+  const lastChar = stem[stem.length - 1];
+  const code = lastChar.charCodeAt(0) - 44032;
+  const jong = code % 28;
+
+  // fără batchim
+  if(jong === 0){
+    if(stem.endsWith("하")){
+      return stem.slice(0, -1) + "해요";
+    }
+
+    if("ㅏㅗ".includes(lastChar)){
+      return stem + "아요";
+    }
+
+    return stem + "어요";
+  }
+
+  // cu batchim
+  return stem + "어요";
+}
+
+function conjugateVerb(verb, tense){
+  if(tense === "present"){
+    return presentPolite(verb);
+  }
   return verb;
 }
