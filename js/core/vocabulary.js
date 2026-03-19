f7d671c9ae45864b20653451d9717ecb6e4f3c75
raluca-korean <raluca.elena.macovei@gmail.com>
2026-03-19 18:32:50 +0200

    Update vocabulary.js

--- js/core/vocabulary.js
+++ js/core/vocabulary.js
@@ -1,1 +1,23 @@
+let GLOBAL_VOCAB = null;
 
+async function loadVocabulary(){
+
+  try{
+
+    const res = await fetch("./data/vocab-korean.json");
+
+    if(!res.ok){
+      throw new Error("NU se poate încărca vocabularul");
+    }
+
+    GLOBAL_VOCAB = await res.json();
+
+    console.log("VOCAB LOADED:", GLOBAL_VOCAB);
+
+    return GLOBAL_VOCAB;
+
+  }catch(err){
+    console.error("VOCAB ERROR:", err);
+  }
+
+}
