d09e64b47b9c255393321d627274038ae5292bea
raluca-korean <raluca.elena.macovei@gmail.com>
2026-03-20 11:16:59 +0200

    Update builder-page.js

--- js/pages/builder-page.js
+++ js/pages/builder-page.js
@@ -34,3 +34,31 @@ function initBuilderPage(){
 }
 console.log("VOCAB:", GLOBAL_VOCAB);
 console.log("INDEX:", dictionaryIndex);
+function updateSentence(key, value){
+
+  const s = sentences[0];
+
+  if(key === "place"){
+    if(!s.places.includes(value)){
+      s.places.push(value);
+    }
+  }
+
+  else if(key === "object"){
+    if(!s.objects.includes(value)){
+      s.objects.push(value);
+    }
+  }
+
+  else if(key === "verb"){
+    if(!s.verbs.includes(value)){
+      s.verbs.push(value);
+    }
+  }
+
+  else {
+    s[key] = value;
+  }
+
+  renderAll();
+}
