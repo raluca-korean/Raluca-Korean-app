b56b16a81a1ea39f70ebdaaabf0ab7f8b904797d
raluca-korean <raluca.elena.macovei@gmail.com>
2026-04-08 16:56:27 +0300

    Update vocab-data.js

--- js/core/vocab-data.js
+++ js/core/vocab-data.js
@@ -1,1 +1,46 @@
 
+window.VocabData = {
+
+  subject: [
+    { ko: "저", ro: "eu", level: 1, freq: 100 },
+    { ko: "너", ro: "tu", level: 1, freq: 90 },
+    { ko: "그", ro: "el/ea", level: 1, freq: 80 },
+    { ko: "우리", ro: "noi", level: 1, freq: 85 }
+  ],
+
+  verb: [
+    { ko: "가다", ro: "a merge", level: 1, freq: 100 },
+    { ko: "먹다", ro: "a mânca", level: 1, freq: 100 },
+    { ko: "마시다", ro: "a bea", level: 1, freq: 95 },
+    { ko: "공부하다", ro: "a studia", level: 1, freq: 90 }
+  ],
+
+  object: [
+    { ko: "음식", ro: "mâncare", level: 1, freq: 100 },
+    { ko: "커피", ro: "cafea", level: 1, freq: 95 },
+    { ko: "책", ro: "carte", level: 1, freq: 90 }
+  ],
+
+  place: [
+    { ko: "집", ro: "acasă", level: 1, freq: 100 },
+    { ko: "학교", ro: "școală", level: 1, freq: 95 },
+    { ko: "카페", ro: "cafenea", level: 1, freq: 90 }
+  ],
+
+  time: [
+    { ko: "오늘", ro: "azi", level: 1, freq: 100 },
+    { ko: "어제", ro: "ieri", level: 1, freq: 95 },
+    { ko: "내일", ro: "mâine", level: 1, freq: 95 }
+  ],
+
+  mod: [
+    { ko: "빨리", ro: "repede", level: 1, freq: 90 },
+    { ko: "천천히", ro: "încet", level: 1, freq: 85 }
+  ],
+
+  conjug: [
+    { ko: "-아요/어요", ro: "prezent", level: 1 },
+    { ko: "-았어요/었어요", ro: "trecut", level: 1 },
+    { ko: "-고 싶어요", ro: "vreau să", level: 1 }
+  ]
+};
